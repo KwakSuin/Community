06890ca925b38114bee111edfc507b871c233cd0
KwakSuin <mygirls0@naver.com>
2020-10-24 11:27:01 +0900

    community

--- app/src/main/java/kwaksuin/portfolio/community/board/quest/QuestFragment.java
+++ app/src/main/java/kwaksuin/portfolio/community/board/quest/QuestFragment.java
@@ -4,47 +4,129 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.google.android.material.tabs.TabLayout;
+import com.pedro.library.AutoPermissions;
+import com.pedro.library.AutoPermissionsListener;
+
+import java.util.ArrayList;
 
 import kwaksuin.portfolio.community.R;
 
-public class QuestFragment extends Fragment {
+public class QuestFragment extends Fragment implements OnDatabaseCallback, AutoPermissionsListener {
+    private static final String TAG ="QuestFragment";
 
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    Toolbar toolbar;
 
-    private String mParam1;
-    private String mParam2;
+    Quest_write write;
+    Quest_list list;
 
-    public QuestFragment() {
-        // Required empty public constructor
-    }
-
-    public static QuestFragment newInstance(String param1, String param2) {
-        QuestFragment fragment = new QuestFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-
-        }
-    }
+    QuestDataBase database;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_quest, container, false);
+
+        final ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_quest,container,false);
+
+        toolbar = rootView.findViewById(R.id.toolbar);
+        write = new Quest_write();
+        list = new Quest_list();
+
+        getFragmentManager().beginTransaction().replace(R.id.container,write).commit();
+
+        TableLayout tabs = rootView.findViewById(R.id.tabs);
+        //tabs.addTab(tabs.newTab().setText("입력"));
+        //tabs.addTab(tabs.newTab().setText("조회"))
+        tabs.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+                Log.d("MainActivity", "선택된 탭 : " + position);
+
+                Fragment selected = null;
+                if (position == 0) {
+                    selected = write;
+                } else if (position == 1) {
+                    selected = list;
+                }
+
+                getFragmentManager().beginTransaction().replace(R.id.container, selected).commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) { }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) { }
+        });
+
+        // open database
+        if (database != null) {
+            database.close();
+            database = null;
+        }
+
+        database = QuestDataBase.getInstance(getContext());
+        boolean isOpen = database.open();
+        if (isOpen) {
+            Log.d(TAG, "Quest database is open.");
+        } else {
+            Log.d(TAG, "Quest database is not open.");
+        }
+
+        AutoPermissions.Companion.loadAllPermissions(getActivity(), 101);
+
+        return rootView;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[],
+                                           int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        AutoPermissions.Companion.parsePermissions(getActivity(), requestCode, permissions, this);
+    }
+
+    //@Override
+    public void onDenied(int requestCode, String[] permissions) {
+        Toast.makeText(getContext(), "permissions denied : " + permissions.length,
+                Toast.LENGTH_LONG).show();
+    }
+
+    // @Override
+    public void onGranted(int requestCode, String[] permissions) {
+        Toast.makeText(getContext(), "permissions granted : " + permissions.length, Toast.LENGTH_LONG).show();
+    }
+
+
+    public void onDestroy() {
+        // close database
+        if (database != null) {
+            database.close();
+            database = null;
+        }
+
+        super.onDestroy();
+    }
+
+    @Override
+    public void insert(String title, String name, String contents) {
+        database.insertRecord(title, name, contents);
+        Toast.makeText(getContext(), "게시글을 추가했습니다.", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public ArrayList<QuestInfo> selectAll() {
+        ArrayList<QuestInfo> result = database.selectAll();
+        Toast.makeText(getContext(), "게시글 목록입니다.", Toast.LENGTH_LONG).show();
+
+        return result;
     }
 }
