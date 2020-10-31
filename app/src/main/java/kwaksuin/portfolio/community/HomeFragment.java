56f68e480f865265cead6dc0ae7a48d8681f1e8b
KwakSuin <mygirls0@naver.com>
2020-10-31 18:38:44 +0900

    community

--- app/src/main/java/kwaksuin/portfolio/community/HomeFragment.java
+++ app/src/main/java/kwaksuin/portfolio/community/HomeFragment.java
@@ -18,20 +18,11 @@ import kwaksuin.portfolio.community.board.photo.PhotoFragment;
 import kwaksuin.portfolio.community.board.quest.QuestFragment;
 
 public class HomeFragment extends Fragment {
-    FreeFragment free;
-    QuestFragment quest;
-    PhotoFragment photo;
-    MarketFragment market;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         final ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_home,container,false);
 
-        TextView FreeBoard = rootView.findViewById(R.id.home_FreeBoard);
-        TextView PhotoBoard = rootView.findViewById(R.id.home_PhotoBoard);
-        TextView QuestBoard = rootView.findViewById(R.id.home_QuestBoard);
-        TextView MarketBoard = rootView.findViewById(R.id.home_MarketBoard);
 
         /** 게시판 이름 누르면 해당 게시판으로 이동 **/
 
